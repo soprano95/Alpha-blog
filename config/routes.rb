95d39cca8538e0e04363cf65d2feb6800839ae03
Ibnbaz <sacko202@gmail.com>
2017-02-01 18:14:43 +0000

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
   get 'edit', to: 'users#edit'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
